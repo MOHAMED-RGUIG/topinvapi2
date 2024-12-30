1c64f4377c2b720eed53698c11aaac38cdef728b
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-12-30 22:53:46 +0100

    Update validInvRoute.js

--- routes/validInvRoute.js
+++ routes/validInvRoute.js
@@ -24,13 +24,13 @@ router.post('/validInvInsert', async (req, res) => {
             request.input('ITMREF_0', sql.NVarChar(), ITMREF_0);
             request.input('LOT_0', sql.NVarChar(), row.LOT_0);
             request.input('STOFCY_0', sql.NVarChar(), row.STOFCY_0);
-            request.input('QTYINV_0', sql.Int, row.Qt);
+            request.input('QTYINV_0', sql.Int, row.STOCOU_0);
             request.input('userID', sql.Int, currentUser.ID); // Make sure userID is passed as a parameter
             console.log(row.Qt);
             // Use parameterized query to insert the data
             await request.query(
                 `INSERT INTO TCE.YINMEN (REFINV_0, ITMREF_0, LOT_0, STOFCY_0, QTYINV_0, [USER]) 
-                 VALUES (@REFINV_0, @ITMREF_0, @LOT_0, @STOFCY_0, @QTYINV_0, @userID)`
+                 VALUES (@REFINV_0, @ITMREF_0, @LOT_0, @STOFCY_0, @STOCOU_0, @userID)`
             );
         }
 
